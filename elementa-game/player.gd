a5bd69bbf9aa6a459a9770afe26454a1d05fdd42
Calian Dalari <141581151+dalari-calian@users.noreply.github.com>
2025-04-07 21:51:58 -0300

    Adiciona novas animações, partes do mapa e teclas de movimentacao
    Adiciona nova movimentação de funcionando invertendo a animação de correr para direita/esquerda e adiciona novas animações de pulo. Nova configuração de teclas de movimentação do jogo

--- elementa-game/player.gd
+++ elementa-game/player.gd
@@ -1,21 +1,18 @@
 extends CharacterBody2D
 
-
 const SPEED = 300.0
-const JUMP_VELOCITY = -400.0
-
+const JUMP_VELOCITY = -600.0
 
 func _physics_process(delta: float) -> void:
-	# Add the gravity.
+	# Adiciona gravidade
 	if not is_on_floor():
 		velocity += get_gravity() * delta
 
-	# Handle jump.
+	# Pulo
 	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
 
-	# Get the input direction and handle the movement/deceleration.
-	# As good practice, you should replace UI actions with custom gameplay actions.
+	# Direção do movimento
 	var direction := Input.get_axis("ui_left", "ui_right")
 	if direction:
 		velocity.x = direction * SPEED
@@ -23,3 +20,21 @@ func _physics_process(delta: float) -> void:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
 
 	move_and_slide()
+
+	# Atualização das sprites
+	if not is_on_floor():
+		# No ar -> sprite de pulo
+		$RunSprite.visible = false
+		$JumpSprite.visible = true
+		$JumpSprite.play("jump")  # Certifique-se que a animação se chama "jump"
+	else:
+		# No chão -> sprite de correr
+		$JumpSprite.visible = false
+		$RunSprite.visible = true
+		
+		# Direção da sprite de corrida
+		if direction != 0:
+			$RunSprite.flip_h = direction < 0
+			$RunSprite.play("anim")  # Certifique-se que a animação se chama "run"
+		else:
+			$RunSprite.play("idle")  # Opcional: animação parado
