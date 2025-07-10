a5f6e5cab55f27978a9d328c6077be6306cb4c8e
Machadomq <gbrielmq1213@gmail.com>
2025-07-09 21:01:31 -0300

    colocando novas animaçoes

--- elementa-game/ataque.gd
+++ elementa-game/ataque.gd
@@ -1,3 +1,4 @@
+#ataque.gd
 extends AnimatedSprite2D
 
 
--- elementa-game/levels/run_sprite.gd
+++ elementa-game/levels/run_sprite.gd
@@ -6,7 +6,7 @@ func _ready() -> void:
 	pass
 
 func _process(delta: float) -> void:
-	
+
 	if Input.is_action_pressed("ui_right"):
 		flip_h = false  
 	elif Input.is_action_pressed("ui_left"):
