57c7db1527bb7d46f67a9c1054baf7a450c9fc98
Guilherme Ehlert <guiehlerts@gmail.com>
2025-12-09 20:00:03 -0300

    Commit
    Musicas das fases

--- elementa-game/menus/main_menu/main.gd
+++ elementa-game/menus/main_menu/main.gd
@@ -6,19 +6,15 @@ extends Node
 
 func _on_play_button_pressed() -> void:
 	get_tree().change_scene_to_file("res://levels/world_02.tscn")
-	Menubutton.play()
 
 
 func _on_credit_button_pressed() -> void:
 	get_tree().change_scene_to_file("res://menus/main_menu/submenus/credits_menu/credits.tscn")
-	Menubutton.play()
 
 
 func _on_config_button_pressed() -> void:
 	get_tree().change_scene_to_file("res://menus/main_menu/submenus/config_menu/config.tscn")
-	Menubutton.play()
 
 
 func _on_exit_button_pressed() -> void:
 	get_tree().quit()
-	Menubutton.play()
